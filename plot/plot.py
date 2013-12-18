efbb48e2617291d85665f19db4a2fb7d569b0894
Philip Hansen <philmeister1994@gmail.com>
2013-12-18 23:19:12 +0100

    Added label and title

--- plot/plot.py
+++ plot/plot.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 x = np.linspace(-1*np.pi,1*np.pi,500)
 y = np.cos(np.square(x))
 
-plt.plot(x,y)
-
+plt.figure()
+plt.plot(x,y, "r")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("y = cos(square(x))")
 plt.show()
