ca86946c6cbe24452739ec74ccf32fc1082a8ba0
Philip Hansen <philmeister1994@gmail.com>
2013-12-18 16:07:56 +0100

    Initial mod and sources commit, let's go

---
+++ mod/forge/src/main/java/dk/philiphansen/srpmod/ExampleMod.java
@@ -0,0 +1,21 @@
+package dk.philiphansen.srpmod;
+
+import net.minecraft.block.Block;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "SRPmod";
+    public static final String VERSION = "1.0";
+    public static SRPWorldGenerator worldGen = new SRPWorldGenerator();
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		GameRegistry.registerWorldGenerator(worldGen);
+    }
+}
