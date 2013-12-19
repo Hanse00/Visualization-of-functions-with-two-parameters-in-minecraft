ebc0a6594a474ae0ef42c28022d20866738eb201
Philip Hansen <philmeister1994@gmail.com>
2013-12-19 14:02:27 +0100

    Code cleanup

--- plot/plot.py
+++ plot/plot.py
@@ -1,15 +1,16 @@
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 import numpy as np
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
+
 X = np.arange(-np.pi, np.pi, 0.1)
 Y = np.arange(-np.pi, np.pi, 0.1)
 X, Y = np.meshgrid(X, Y)
 Z = np.cos(np.square(X)) + np.sin(np.square(Y))
+
 surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0)
 
 plt.show()
