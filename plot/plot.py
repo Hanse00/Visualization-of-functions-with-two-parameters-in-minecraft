c7151060a7f5f882f88d7ba700dfb643d2c7169d
Philip Hansen <philmeister1994@gmail.com>
2013-12-19 14:00:59 +0100

    Edited code to show my function

--- plot/plot.py
+++ plot/plot.py
@@ -6,11 +6,10 @@ import numpy as np
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
+X = np.arange(-np.pi, np.pi, 0.1)
+Y = np.arange(-np.pi, np.pi, 0.1)
 X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
+Z = np.cos(np.square(X)) + np.sin(np.square(Y))
 surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, linewidth=0)
 
 plt.show()
