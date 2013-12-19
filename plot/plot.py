0d0508ee7499c7343ee12108b15e389781696c6d
Philip Hansen <philmeister1994@gmail.com>
2013-12-19 13:47:24 +0100

    Plot sin(square(x)) and cos

--- plot/plot.py
+++ plot/plot.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-1*np.pi,1*np.pi,500)
-y = np.cos(np.square(x))
 
 fig, axes = plt.subplots()
-axes.plot(x,y, "r")
+axes.plot(x,np.cos(np.square(x)), "r")
+axes.plot(x,np.sin(np.square(x)), "b")
 axes.set_xlabel("x")
 axes.set_ylabel("y")
 axes.set_title("y = cos(square(x))")
