a8b7120e1fe65a4f9642827240b4058bbf0644be
Philip Hansen <philmeister1994@gmail.com>
2013-12-19 13:55:58 +0100

    Copied 3D demo

--- plot/plot.py
+++ plot/plot.py
@@ -1,12 +1,23 @@
-import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
+import numpy as np
 
-x = np.linspace(-1*np.pi,1*np.pi,500)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+        linewidth=0, antialiased=False)
+ax.set_zlim(-1.01, 1.01)
 
-fig, axes = plt.subplots()
-axes.plot(x,np.cos(np.square(x)), "r")
-axes.plot(x,np.sin(np.square(x)), "b")
-axes.set_xlabel("x")
-axes.set_ylabel("y")
-axes.set_title("y = cos(square(x))")
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
 plt.show()
