e6bdab2d7f5857b96ed4eb88ceb831f591563a14
Philip Hansen <philmeister1994@gmail.com>
2013-12-19 13:58:35 +0100

    removed unnecessary code

--- plot/plot.py
+++ plot/plot.py
@@ -11,13 +11,6 @@ Y = np.arange(-5, 5, 0.25)
 X, Y = np.meshgrid(X, Y)
 R = np.sqrt(X**2 + Y**2)
 Z = np.sin(R)
-surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
-        linewidth=0, antialiased=False)
-ax.set_zlim(-1.01, 1.01)
-
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-fig.colorbar(surf, shrink=0.5, aspect=5)
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, linewidth=0)
 
 plt.show()
