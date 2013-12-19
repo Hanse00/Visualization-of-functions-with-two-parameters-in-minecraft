92eb9b8c22bfdad0b771b2530752a3ad6344c62a
Philip Hansen <philmeister1994@gmail.com>
2013-12-19 14:32:17 +0100

    Added loops to generate surface

--- mod/forge/src/main/java/dk/philiphansen/srpmod/SRPWorldGenerator.java
+++ mod/forge/src/main/java/dk/philiphansen/srpmod/SRPWorldGenerator.java
@@ -10,7 +10,11 @@ public class SRPWorldGenerator implements IWorldGenerator {
 
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
-		world.setBlock(chunkX*16, 64, chunkZ*16 + 1, 5);
+		for (int i = 1; i <= 16; i++) {
+			for (int j = 1; j <= 16; j++) {
+				world.setBlock(chunkX*16 + i, 64, chunkZ*16 + j, 5);
+			}
+		}
 	}
 
 }
